71b5d44207885667e62d0c604ddfd464c3511814
jmqlwyb <maglawayb@gmail.com>
2024-10-03 09:10:07 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <center>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/"> </router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item ">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view></router-view>
+    </center>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.navbar {
+  justify-content: center;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <center>
+      <h1>Home Page</h1>
+      <p>𝘞𝘦𝘭𝘤𝘰𝘮𝘦 𝘵𝘰 𝘵𝘩𝘦 𝘩𝘰𝘮𝘦 𝘱𝘢𝘨𝘦</p>
+    
+     
+    </center>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
